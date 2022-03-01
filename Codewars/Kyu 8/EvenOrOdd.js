c0cb3f0e90e4cf204919183412db11a70015acd8
hazrinatn <hazrinatn@gmail.com>
2022-03-01 09:16:16 +0700

    Add even or odd coding challenge

---
+++ Codewars/Kyu 8/EvenOrOdd.js
@@ -0,0 +1,17 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+//P: Given an integer. How about 0?
+//R: Return 'Even' for even numbers or 'Odd' for odd numbers
+//E: If we were given 111 -> return 'Odd'
+//                    8888 -> return 'Even'
+//                    0 -> return 'Even'
+//P: Write a function that takes number as a parameter
+function evenOrOdd(num) {
+    //write conditionals. Return 'even' when it's exactly divisible by 2, else return 'odd'
+    return num % 2 === 0 ? 'Even' : 'Odd';
+}
+
+console.log(evenOrOdd(0)) // should return even
+console.log(evenOrOdd(111)) // should return odd
+console.log(evenOrOdd(8888)) // should return even
+
