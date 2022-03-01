88a03ad1ecb0dc0ee7c500d752ed91b4b5d0053e
hazrinatn <hazrinatn@gmail.com>
2022-03-01 09:19:36 +0700

    Add edge cases

--- Codewars/Kyu 8/EvenOrOdd.js
+++ Codewars/Kyu 8/EvenOrOdd.js
@@ -1,10 +1,11 @@
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
-//P: Given an integer. How about 0?
+//P: Given an integer. How about 0? How about negative numbers?
 //R: Return 'Even' for even numbers or 'Odd' for odd numbers
 //E: If we were given 111 -> return 'Odd'
 //                    8888 -> return 'Even'
 //                    0 -> return 'Even'
+//                    -12345 -> return 'Odd'
 //P: Write a function that takes number as a parameter
 function evenOrOdd(num) {
     //write conditionals. Return 'even' when it's exactly divisible by 2, else return 'odd'
@@ -14,4 +15,6 @@ function evenOrOdd(num) {
 console.log(evenOrOdd(0)) // should return even
 console.log(evenOrOdd(111)) // should return odd
 console.log(evenOrOdd(8888)) // should return even
+console.log(evenOrOdd(-12345)) // should return odd
+
 
