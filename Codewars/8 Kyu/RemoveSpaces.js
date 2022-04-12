e75a206a50a610dbb4c073f876d520e09be3e698
hazrinatn <hazrinatn@gmail.com>
2022-04-12 16:48:46 +0700

    Add remove spaces from string coding challenge

--- Codewars/8 Kyu/RemoveSpaces.js
+++ Codewars/8 Kyu/RemoveSpaces.js
@@ -1,2 +1,17 @@
 // Remove String Spaces
 // Simple, remove the spaces from the string, then return the resultant string.
+
+//P: Given a string, containing space(s)
+//R: Return the string without the spaces
+//E: If we were given "Hello World" -> return "HelloWorld"
+//                    "Da ra ri" -> return "Darari"
+//                    "Too cool for school" -> return "Toocoolforschool"
+//P: Make a function that takes a string as the parameter
+function removeSpaces(str) {
+   return str.split(" ").join("")
+}
+
+console.log(removeSpaces("Hello World"));// should return "HelloWorld"
+console.log(removeSpaces("Da ra ri"));// should return "Darari"
+console.log(removeSpaces("Too cool for school"));// should return "Toocoolforschool"
+
