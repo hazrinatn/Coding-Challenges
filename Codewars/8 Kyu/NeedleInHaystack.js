08d07a76859980f7ba34dd7d6dd00b625886de3f
hazrinatn <hazrinatn@gmail.com>
2022-04-25 21:01:57 +0700

    Add needle in haystack coding challenge

---
+++ Codewars/8 Kyu/NeedleInHaystack.js
@@ -0,0 +1,35 @@
+// Can you find the needle in the haystack?
+
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message (as a string) that says:
+
+// "found the needle at position " plus the index it found the needle, so:
+
+// findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
+// should return "found the needle at position 5"
+
+
+//P: Given an array of strings
+//R: Return the position of "needle" in the array
+//E: If we were given ['needle', 'junk', 'moreJunk'] -> return "found the needle at position 0"
+//                    ['hay', 'needle', 'junk', 'moreJunk'] -> return "found the needle at position 1"
+//                    ['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'] -> return "found the needle at position 5"
+//P: Make a function that takes an array as the parameter
+function findNeedle (arr) {
+    let position;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == 'needle') {
+            position = i
+        }
+    }
+
+    return(`found the needle at position ${position}`);
+}
+
+console.log(findNeedle(['needle', 'junk', 'moreJunk']));// should return "found the needle at position 0"
+console.log(findNeedle(['hay', 'needle', 'junk', 'moreJunk']));// should return "found the needle at position 1"
+console.log(findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk']));// should return "found the needle at position 5"
+
+
+
