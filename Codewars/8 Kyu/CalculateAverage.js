3ab6947f67353fdb3a254183334eaa0a87b67c80
hazrinatn <hazrinatn@gmail.com>
2022-05-02 22:21:11 +0700

    Add alternative solution

--- Codewars/8 Kyu/CalculateAverage.js
+++ Codewars/8 Kyu/CalculateAverage.js
@@ -10,11 +10,21 @@
 //P: Make a function that take an array as the parameter
 // sum all the numbers and then divide by the count of the number
 function calculateAverage(arr) {
-    let total = arr.reduce((sum, count) => sum + count, 0)
-    let average = total/arr.length
-    return average
+    if (arr.length < 1) {
+        return 0
+    } else {
+        let total = arr.reduce((sum, count) => sum + count, 0)
+        let average = total/arr.length
+        return average
+    }  
 }
 
 console.log(calculateAverage([1, 2, 3, 4])); //should return 2.5
 console.log(calculateAverage([12, 2, 8, 9])); //should return 7.75
 console.log(calculateAverage([1, 2, 3, 4, 5])); //should return 3
+console.log(calculateAverage([])); //should return 0
+
+//Alternative Solution
+// const find_average = (array) => {  
+//     return array.length === 0 ? 0 : array.reduce((acc, ind)=> acc + ind, 0)/array.length
+// }
