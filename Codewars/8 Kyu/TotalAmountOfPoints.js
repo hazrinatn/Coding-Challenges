17218287e6ec11f299f0dc8597efe46107b498a0
hazrinatn <hazrinatn@gmail.com>
2022-05-19 06:39:08 +0700

    Add alternative solution for TotalAmountOfPoints.js

--- Codewars/8 Kyu/TotalAmountOfPoints.js
+++ Codewars/8 Kyu/TotalAmountOfPoints.js
@@ -54,3 +54,8 @@ function points(games) {
 console.log(points(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"])); // return 30
 console.log(points(["1:1","2:2","3:3","4:4","2:2","3:3","4:4","3:3","4:4","4:4"])); // return 10
 console.log(points(["0:1","0:2","0:3","0:4","1:2","1:3","1:4","2:3","2:4","3:4"])); // return 0
+
+//Alternative Solution
+// const points = games => games.reduce((output,current)=>{
+//     return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
+// }, 0)
