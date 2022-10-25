cdc3476246c0ebec6048f811f324ade5625d372f
hazrinatn <hazrinatn@gmail.com>
2022-10-26 05:48:44 +0700

    Add regular ball super ball coding challenge

---
+++ Codewars/8 Kyu/RegularBallSuperBall.js
@@ -0,0 +1,41 @@
+/*
+Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+ball1 = new Ball();
+ball2 = new Ball("super");
+
+ball1.ballType     //=> "regular"
+ball2.ballType     //=> "super"
+
+PREP
+Parameters:
+Given an object of a class Ball that has ball type property of "ball type"
+
+Results:
+If no argument given, instantiate ball objects with "ball type" of "regular", otherwise instantiate ball objects accordingly
+
+Examples:
+If we were given new Ball().ballType -> should return "regular"
+                 new Ball("super").ballType -> should return "super"
+
+Pseudocodes:
+Make a class that has property of ballType
+Instantiate new ball objects
+*/
+
+let Ball = function(ballType = "regular") {
+    this.ballType = ballType;
+};
+
+//Alternative Solution
+// let Ball = function(ballType) {
+//     this.ballType = ballType || 'regular';
+// };
+
+let ball1 = new Ball();
+let ball2 = new Ball("super")
+
+console.log(ball1);
+console.log(ball2);
