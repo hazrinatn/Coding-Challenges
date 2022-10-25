b26bb382ff9cad7a7d68734eeec7b4ee6f5c33da
hazrinatn <hazrinatn@gmail.com>
2022-10-25 22:59:27 +0700

    Add filter out the geese coding challenge

---
+++ Codewars/8 Kyu/FilterOutTheGeese.js
@@ -0,0 +1,41 @@
+/*
+Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
+
+The geese are any strings in the following array, which is pre-populated in your solution:
+
+  ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+For example, if this array were passed as an argument:
+
+ ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+Your function would return the following array:
+
+["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
+
+PREP
+Parameters:
+Given an array of strings and a geese variable ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+
+Results:
+Return filtered array containing strings except those that match string in geese
+
+Example:
+If we were given ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"] -> should return ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+                 ["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"] -> should return ["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"]
+                 ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]) -> should return []
+
+Pseudocodes:
+Make a function that takes an array as the parameter
+Filter the array
+Return the filtered array
+*/
+
+function gooseFilter (birds) {
+    let geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    return birds.filter(b => !geese.includes(b))
+};
+
+console.log(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]));
+console.log(gooseFilter(["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"]));
+console.log(gooseFilter(["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]));
+
