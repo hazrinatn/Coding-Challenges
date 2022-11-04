aba5edc9edb18e20297cc80b38be57f9f5616b92
hazrinatn <hazrinatn@gmail.com>
2022-11-04 18:57:40 +0700

    Add replace dots coding challenge

---
+++ Codewars/8 Kyu/ReplaceDots.js
@@ -0,0 +1,20 @@
+/*
+The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+But it's not working properly.
+
+Task
+Fix the bug so we can all go home early.
+
+Notes
+String str will never be null.
+*/
+
+// Use RegEx
+let replaceDots = function(str) {
+    // added the \ to escape special characters
+    // added the g so that replace is run for all occurences in the string
+    return str.replace(/\./g, '-');
+}
+
+console.log(replaceDots("one.two.three"));
