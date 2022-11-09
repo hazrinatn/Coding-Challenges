736fb2108fd4a0e7254b3ba7d6646ff0d60d266f
hazrinatn <hazrinatn@gmail.com>
2022-11-09 08:36:37 +0700

    Add faster solution for ArrayDiff.js

--- Codewars/6 Kyu/ArrayDiff.js
+++ Codewars/6 Kyu/ArrayDiff.js
@@ -39,6 +39,12 @@ const arrayDiff = (a,b) => {
 //     return a.filter(function(x) { return b.indexOf(x) == -1; });
 //   }
 
+// Faster Solution using Set
+// function array_diff(a, b) {
+//     b = new Set(b)
+//     return a.filter(v => !b.has(v))
+// }
+
 console.log(arrayDiff([1,2], [1]));
 console.log(arrayDiff([1,2,2], [1]));
 console.log(arrayDiff([1,2,2], [2]));
