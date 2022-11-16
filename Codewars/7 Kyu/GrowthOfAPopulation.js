7d5523cd895a3123999124f21366488370c330eb
hazrinatn <hazrinatn@gmail.com>
2022-11-16 19:32:29 +0700

    Refactor GrowthOfAPopulation.js

--- Codewars/7 Kyu/GrowthOfAPopulation.js
+++ Codewars/7 Kyu/GrowthOfAPopulation.js
@@ -53,7 +53,7 @@ return the result
 function nbYear(p0, percent, aug, p) {
     let count = 0;
     while (p0 < p) {
-        p0 = p0 + p0 * percent/100 + aug
+        p0 = Math.floor(p0 + p0 * percent/100 + aug)
         count++
     }
     return count
