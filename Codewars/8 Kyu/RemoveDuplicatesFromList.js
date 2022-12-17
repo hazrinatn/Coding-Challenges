3327c9f43fb37f1eea2bd926afb149d8fe00c1af
hazrinatn <hazrinatn@gmail.com>
2022-12-18 05:23:59 +0700

    Add remove duplicates from list coding challenge

---
+++ Codewars/8 Kyu/RemoveDuplicatesFromList.js
@@ -0,0 +1,31 @@
+/*
+Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+
+PREP
+Parameters:
+Given an array of integers, can be positive, can be negative, can be 0
+
+Results:
+Remove duplicates from the array and return it
+
+Examples:
+If we were given [1,3,5,5] -> return [1,3,5]
+                 [12,22,18,18,12] -> return [12,22,18]
+                 [-4,0,0,4,5] -> [-4,0,4,5]
+
+Pseudocodes:
+Make a function that takes an array as the parameter
+Make a new Set
+Turn the set into an array
+Return the array
+*/ 
+
+const distinct = (a) => {
+    return [... new Set(a)]
+}
+
+console.log(distinct([1,3,5,5]));
+console.log(distinct([12,22,18,18,12]));
+console.log(distinct([-4,0,0,4,5]));
