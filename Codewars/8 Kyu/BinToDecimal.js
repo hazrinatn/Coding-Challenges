4094e1b87e17a01019c9298bb2f344567465db5c
hazrinatn <hazrinatn@gmail.com>
2022-12-20 20:31:43 +0700

    Add binary to decimal coding challenge

---
+++ Codewars/8 Kyu/BinToDecimal.js
@@ -0,0 +1,9 @@
+/*
+Complete the function which converts a binary number (given as a string) to a decimal number.
+*/ 
+
+const binToDec = (bin) => {
+    return parseInt(bin, 2)
+}
+
+console.log(binToDec("1001001"));
